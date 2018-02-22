f454e5e7c9e09bf5fa5ab7b606a91dd1d0c9846d
lidianvilazio <lidianvilazio@hotmail.com>
2018-02-22 11:32:53 -0500

    My OO Counting Sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    counter = 0
+    string = self.split(" ")
+    string.each do |i|
+      if i.question? || i.exclamation? || i.sentence?
+        counter += 1
+      end
+    end
+    counter
   end
+
+  # binding.pry
 end
