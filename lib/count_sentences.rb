39039a7404908e833b2d589f8d75b3843fd6f901
Barney Fabian <bafa@tutanota.de>
2018-02-24 04:32:09 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,19 +1,17 @@
-require 'pry'
-
 class String
-
+  
   def sentence?
-    self.end_with?(".") ? true : false
+    end_with?(".") ? true : false
   end
-
+  
   def question?
-    self.end_with?("?") ? true : false
+    end_with?("?") ? true : false
   end
-
+  
   def exclamation?
-    self.end_with?("!") ? true : false
+    end_with?("!") ? true : false
   end
-
+  
   def count_sentences
     self.split(/[.?!]/).reject do |x| x.empty? end.length
   end
